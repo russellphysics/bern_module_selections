d13fd33767c7d164affc48384fbd882fa468b76d
Elise Dianne Hinkle <ehinkle@login03.chn>
2023-08-18 08:14:02 -0700

    Adding rudimentary event display capabilities in /common.

--- proton_selection/module0_flow_selection/proton_selection.py
+++ proton_selection/module0_flow_selection/proton_selection.py
@@ -55,9 +55,24 @@ class ProtonSelection(H5FlowStage):
         ext_trigs_dset_name='charge/ext_trigs',
         path='high_purity_sel/protons') # path within hdf5 file vs. file path
 
-    event_sel_dset_name = 'event_sel_reco'
+    event_summ_dset_name = 'event_summ_reco'
+    event_tracks_dset_name = 'event_tracks_reco'
+    #event_hits_dset_name = 'event_hits_reco'
 
-    event_sel_dtype = np.dtype([('max_track_length', 'f8')])
+    event_summ_dtype = np.dtype([('sel', 'u1'),
+                                 ('event_id', 'f8'),
+                                 ('next_trigs', 'f8'),
+                                 ('ntracks', 'f8'),
+                                 ('nhits', 'f8'), 
+                                 ('event_charge', 'f8')])
+
+    event_tracks_dtype = np.dtype([('nhits', 'f8'),
+                                   ('length', 'f8'),
+                                   ('theta', 'i8'),
+                                   ('phi', 'f8'),
+                                   ('track_q', 'f8')])
+#
+    #hit_profile_dtype = np.dtype([('idx','i4'),('rr','f8')])
 
 
     def __init__(self, **params):
@@ -88,13 +103,18 @@ class ProtonSelection(H5FlowStage):
         attrs = dict()
         for key in self.default_params:
             attrs[key] = getattr(self, key)
-        print(attrs)
+        #print(attrs)
         self.data_manager.set_attrs(self.path,
                                     classname=self.classname,
                                     class_version=self.class_version,
                                     **attrs)
-        self.data_manager.create_dset(f'{self.path}/{self.event_sel_dset_name}',
-                                      self.event_sel_dtype)
+        self.data_manager.create_dset(f'{self.path}/{self.event_summ_dset_name}',
+                                      self.event_summ_dtype)
+        #self.data_manager.create_dset(f'{self.path}/{self.event_tracks_dset_name}',
+        #                              self.event_tracks_dtype)
+        #self.data_manager.create_dset(f'{self.path}/{self.hit_profile_dset_name}',
+        #                              self.hit_profile_dtype)
+        #self.data_manager.create_ref(f'{self.path}/{self.hit_profile_dset_name}', self.hits_dset_name)
         #if self.is_mc:
         #    self.data_manager.create_dset(f'{self.path}/{self.event_sel_truth_dset_name}',
         #                                  self.event_sel_dtype)
@@ -114,16 +134,37 @@ class ProtonSelection(H5FlowStage):
 
     def finish(self, source_name):
         super(ProtonSelection, self).finish(source_name)
-        sel_dset_name = f'{self.path}/{self.event_sel_dset_name}'
+        summ_dset_name = f'{self.path}/{self.event_summ_dset_name}'
 
         if self.rank == 0:
-            #total = len(self.data_manager.get_dset(sel_dset_name))
-            ultimate_max_track_length = np.max(self.data_manager.get_dset(sel_dset_name)['max_track_length'])
-            print("Maximum track length in file:", ultimate_max_track_length, "mm")
+            total = len(self.data_manager.get_dset(summ_dset_name))
+            min_tracks = np.min(self.data_manager.get_dset(summ_dset_name)['ntracks'])
+            print("Minimum tracks in an event:", min_tracks)
+            max_tracks = np.max(self.data_manager.get_dset(summ_dset_name)['ntracks'])
+            print("Maximum tracks in an event:", max_tracks)
+
+            min_hits = np.min(self.data_manager.get_dset(summ_dset_name)['nhits'])
+            print("Minimum hits in an event:", min_hits)
+            max_hits = np.max(self.data_manager.get_dset(summ_dset_name)['nhits'])
+            print("Maximum hits in an event:", max_hits)
+
+            min_charge = np.min(self.data_manager.get_dset(summ_dset_name)['event_charge'])
+            print("Minimum charge in an event:", min_charge)
+            max_charge = np.max(self.data_manager.get_dset(summ_dset_name)['event_charge'])
+            print("Maximum charge in an event:", max_charge)
+
+            min_ext_trigs = np.min(self.data_manager.get_dset(summ_dset_name)['next_trigs'])
+            print("Minimum ext_trigs in an event:", min_ext_trigs)
+            max_ext_trigs = np.max(self.data_manager.get_dset(summ_dset_name)['next_trigs'])
+            print("Maximum ext_trigs in an event:", max_ext_trigs)
+
             #nstopping = np.sum(self.data_manager.get_dset(sel_dset_name)['stop'])
-            #nselected = np.sum(self.data_manager.get_dset(sel_dset_name)['max_track_length'])
+            nselected = np.sum(self.data_manager.get_dset(summ_dset_name)['sel'])
             #print(f'Stopping: {nstopping} / {total} ({nstopping/total:0.03f})')
-            #print(f'Selected: {nselected} / {total} ({nselected/total:0.03f})')
+            print(f'Selected: {nselected} / {total} ({nselected/total:0.03f})')
+            sel_events_mask = self.data_manager.get_dset(summ_dset_name)['sel'] == 1
+            sel_events = self.data_manager.get_dset(summ_dset_name)[sel_events_mask]['event_id'][10:20]
+            print("Sample events:", sel_events)
 
             #if self.is_mc:
             #    sel_truth_dset_name = f'{self.path}/{self.event_sel_truth_dset_name}'
@@ -152,23 +193,81 @@ class ProtonSelection(H5FlowStage):
 
         if events.shape[0]:
 
-                    # prep arrays to write to file
-            #print("Shape of Tracks:",tracks.shape)
-            max_track_length = tracks['length'].max(axis=-1)
+            # prep arrays to write to file
+            event_ids = events['id']
+            event_next_trigs = events['n_ext_trigs']
+            #print("Shape of one event's tracks:", tracks['id'][0].shape)
+            #print("One event's tracks's mask:", tracks['id'][0].mask)
+            #print("One event's tracks's ids:", tracks['id'][0])
+            #print("Number of valid events for one event:", int((~tracks['id'][0].mask).sum()))
+            event_ntracks = np.array([int((~tracks['id'][i].mask).sum()) for i in range(len(tracks))])
+            event_nhits = events['nhit']
+            event_charge = events['q']
+
+            #print("Hits Shape:", hits.shape)
+
+            max_hit_charge = np.array([np.max(hits['q'][i]) for i in range(len(hits))])
+
+            nhits_cut = (event_nhits > 50) & (event_nhits < 5000)
+            hit_charge_threshold_cut = (max_hit_charge > 300) 
+            external_trigger_cut = (event_next_trigs > 0)
+            event_sel = nhits_cut & hit_charge_threshold_cut & external_trigger_cut
+
+            #track_nhits = tracks.ravel()['nhit'][~tracks['nhit'].mask]
+            #track_length = tracks.ravel()['length'][~tracks['length'].mask]
+            #track_theta = tracks.ravel()['theta'][~tracks['theta'].mask]
+            #track_phi = tracks.ravel()['phi'][~tracks['phi'].mask]
+            #track_q = tracks.ravel()['q'][~tracks['q'].mask]
+
+            
+            
+            
             #print("Max Track length:", max_track_length)
 
-        event_sel = np.zeros(len(tracks), dtype=self.event_sel_dtype)
+        event_summ = np.zeros(len(tracks), dtype=self.event_summ_dtype)
 
-        if len(event_sel):
-            event_sel['max_track_length'] = max_track_length
+        if len(event_summ):
+            event_summ['sel'] = event_sel
+            event_summ['event_id'] = event_ids
+            event_summ['next_trigs'] = event_next_trigs
+            event_summ['ntracks'] = event_ntracks
+            event_summ['nhits'] = event_nhits
+            event_summ['event_charge'] = event_charge
 
-                    # reserve data space
-        event_sel_slice = self.data_manager.reserve_data(
-            f'{self.path}/{self.event_sel_dset_name}', source_slice)
+        #event_tracks = np.zeros(len(track_length), dtype=self.event_tracks_dtype)
+
+        #if len(event_tracks):
+        #    event_tracks['nhits'] = track_nhits
+        #    event_tracks['length'] = track_length
+        #    event_tracks['theta'] = track_theta
+        #    event_tracks['phi'] = track_phi
+        #    event_tracks['track_q'] = track_q
+        #    
+        #hit_profile = np.zeros(hits.shape, dtype=self.hit_profile_dtype)
+        #if len(hit_profile):
+        #    hit_profile['idx'] -= 1
+        #    hit_profile['idx'][~hits['id'].mask] = hit_prof_idx[~hits['id'].mask]
+        #    hit_profile['rr'][~hits['id'].mask] = hit_prof_rr[~hits['id'].mask]
+
+
+        # reserve data space
+        event_summ_slice = self.data_manager.reserve_data(
+            f'{self.path}/{self.event_summ_dset_name}', source_slice)
+        #event_tracks_slice = self.data_manager.reserve_data(
+        #    f'{self.path}/{self.event_tracks_dset_name}', source_slice)
+        #event_hits_slice = self.data_manager.reserve_data(
+        #    f'{self.path}/{self.hit_profile_dset_name}', int((~hits['id'].mask).sum()))
+
 
         # write
-        self.data_manager.write_data(f'{self.path}/{self.event_sel_dset_name}',
-                                     event_sel_slice, event_sel)
+        self.data_manager.write_data(f'{self.path}/{self.event_summ_dset_name}',
+                                     event_summ_slice, event_summ)
+        #self.data_manager.write_data(f'{self.path}/{self.event_tracks_dset_name}',
+        #                             event_tracks_slice, event_tracks)
+        #self.data_manager.write_data(f'{self.path}/{self.hit_profile_dset_name}',
+        #                             event_hits_slice, hit_profile[~hits['id'].mask])
+        #self.data_manager.write_ref(f'{self.path}/{self.hit_profile_dset_name}',
+        #        self.hits_dset_name, np.c_[event_hits_slice, hits['id'].compressed()])
 
             ## calculate hit positions and charge
             #hit_q = self.larpix_gain * q['q'] # convert mV -> ke
